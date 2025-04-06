e1532d69d1db1ec7b404098269e675df7765ca3c
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-04-07 01:10:15 +0600

    update

--- src/app/(homeLayout)/page.tsx
+++ src/app/(homeLayout)/page.tsx
@@ -5,7 +5,10 @@ import Category from "@/components/homeComponents/Category";
 import FeaturedFood from "@/components/homeComponents/FeaturedFood";
 import TopRestaurant from "../../components/homeComponents/TopRestaurant";
 // import Support from "@/components/homeComponents/Support";
-import FAQ from "@/components/homeComponents/FAQ";
+// import FAQ from "@/components/homeComponents/FAQ";
+import dynamic from "next/dynamic";
+const FAQ = dynamic(() => import("@/components/homeComponents/FAQ"), { ssr: false });
+
 import CitiesWeServe from "@/components/homeComponents/CitiesWeServe";
 
 export default function Home() {
