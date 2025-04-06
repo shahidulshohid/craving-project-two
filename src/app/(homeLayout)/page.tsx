308e4e2a5a818a2a694aace49e66aea7e313e6db
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-04-06 14:33:41 +0600

    home page update

--- src/app/(homeLayout)/page.tsx
+++ src/app/(homeLayout)/page.tsx
@@ -1,31 +1,43 @@
-"use client"
 
-import Banner from "@/components/homeComponents/banner";
-import Category from "@/components/homeComponents/Category";
-import FeaturedFood from "@/components/homeComponents/FeaturedFood";
-import TopRestaurant from "../../components/homeComponents/TopRestaurant";
-import Support from "@/components/homeComponents/Support";
-import FAQ from "@/components/homeComponents/FAQ";
-import CitiesWeServe from "@/components/homeComponents/CitiesWeServe";
 
-export default function Home() {
+// import Banner from "@/components/homeComponents/banner";
+// import Category from "@/components/homeComponents/Category";
+// import FeaturedFood from "@/components/homeComponents/FeaturedFood";
+// import TopRestaurant from "../../components/homeComponents/TopRestaurant";
+// import Support from "@/components/homeComponents/Support";
+// import FAQ from "@/components/homeComponents/FAQ";
+// import CitiesWeServe from "@/components/homeComponents/CitiesWeServe";
+
+// export default function Home() {
+//   return (
+//     <section className="lg:space-y-20">
+//       {/* bg-amber-100 */}
+//       {/* Banner section */}
+//       <Banner />
+//       {/* Category Section */}
+//       <Category />
+//       {/* Cities We Serve Section */}
+//       <CitiesWeServe></CitiesWeServe>
+//       {/* Featured Food */}
+//       <FeaturedFood />
+//       {/* top restaurant */}
+//       <TopRestaurant />
+//       {/* faq section */}
+//       <FAQ></FAQ>
+//       {/* Support */}
+//       <Support />
+//     </section>
+//   );
+// }
+
+
+
+const page = () => {
   return (
-    <section className="lg:space-y-20">
-      {/* bg-amber-100 */}
-      {/* Banner section */}
-      <Banner />
-      {/* Category Section */}
-      <Category />
-      {/* Cities We Serve Section */}
-      <CitiesWeServe></CitiesWeServe>
-      {/* Featured Food */}
-      <FeaturedFood />
-      {/* top restaurant */}
-      <TopRestaurant />
-      {/* faq section */}
-      <FAQ></FAQ>
-      {/* Support */}
-      <Support />
-    </section>
+    <div>
+      <h1>this is home</h1>
+    </div>
   );
-}
+};
+
+export default page;
