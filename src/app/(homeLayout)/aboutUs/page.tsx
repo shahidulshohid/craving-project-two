331bf00dba4d512d08dfffc8a8c89d04f0e25a85
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-04-07 11:19:40 +0600

    about page update

--- src/app/(homeLayout)/aboutUs/page.tsx
+++ src/app/(homeLayout)/aboutUs/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import dynamic from "next/dynamic";
-const AboutComponent = dynamic(() => import("@/components/AboutComponent"));
+const AboutComponent = dynamic(() => import("@/components/AboutComponent"), {ssr:false});
 
 const page = () => {
   return (
